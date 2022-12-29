058d79ed7f120def9d9faf719c4668fcca26984c
Liz Towoju <liztowoju@gmail.com>
2022-12-29 21:55:56 +0100

    feat : filter complete / uncompleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,58 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 //importing Components
 import Form from './Components/Form.jsx'
 import Todos from './Components/Todos'
 
 function App() {
+
+  // state 
   const [inputText, setInputText] = useState('')
   const [todos, setTodos] = useState([])
   const [status, setStatus] = useState('all')
-  const [filteredTodo, setFilteredTodo] = useState([])
+  const [filteredTodos, setFilteredTodos] = useState([])
 
-  return (
-    <div className='react'>
-      <header>
-        <h1>Liz' Todo List</h1>
-      </header>
-      <Form 
-      todos={todos} 
-      setTodos={setTodos} 
+  // useEffect{
+useEffect(function(){
+filterHandler()
+}, [status, todos])
+
+
+
+//function
+function filterHandler() {
+  switch (status) {
+    case 'completed':
+      setFilteredTodos(todos.filter(function (todo) {
+        return todo.completed
+      }))
+      break;
+    case 'uncompleted':
+      setFilteredTodos(todos.filter(function (todo) {
+        return !todo.completed
+      }))
+      break;
+    default:
+      setFilteredTodos(todos)
+      break;
+  }
+}
+
+return (
+  <div className='react'>
+    <header>
+      <h1>Liz' Todo List</h1>
+    </header>
+    <Form
+      todos={todos}
+      setTodos={setTodos}
       inputText={inputText}
-      setInputText={setInputText} 
+      setInputText={setInputText}
       setStatus={setStatus}
-      />
-      <Todos todos={todos} setTodos={setTodos} />
-    </div >
-  )
+    />
+    <Todos todos={todos} setTodos={setTodos} />
+  </div >
+)
 }
 
 export default App
